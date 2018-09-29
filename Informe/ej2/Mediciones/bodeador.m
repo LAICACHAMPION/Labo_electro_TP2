a5aed0e0f51274e7cc3eed840b9af2a420cc2f6c
taomasgonzalez <togonzalez@itba.edu.ar>
2018-09-29 11:22:35 -0300

    agregue bodes ej 2

--- Informe/ej2/Mediciones/bodeador.m
+++ Informe/ej2/Mediciones/bodeador.m
@@ -2,10 +2,10 @@ clc;
 clearvars;
 clear all;
 
-M = csvread('tp2_ej4_a_c.csv')
+M = csvread('ej2_labo.csv');
 
-f = M(:,1)*1000;
-Vin = M(:,2);
+f = M(:,1)*10000;
+Vin = M(:,2)/10;
 Vout = M(:,3);
 relacion = Vout /Vin;
 dB = 20*log(relacion);
@@ -16,26 +16,26 @@ fig_mod =figure;
 modulo_plot = plot(f, dB);
 ax = gca;
 ax.XScale = 'log';
-ax.XLim = [1000 1000000];
+ax.XLim = [ax.XLim(1)*10 ax.XLim(2)];
 ax.Title.String = 'Respuesta en Frecuencia - Módulo';
-ax.XLabel = '' 
-
-
+ax.XLabel.String = 'Frecuencia (Hz)';
+ax.YLabel.String = 'Magnitud (dB)';
 
 figure;
 fase_plot = plot(f, wrapTo180(fase));
 ax = gca
-ax.XLim = [ax.XLim(1)*10 ax.XLim(2)];
+ax.XLim = [ax.XLim(1)*10000 ax.XLim(2)];
 ax.XScale = 'log';
 ax.Title.String = 'Respuesta en Frecuencia - Fase';
-
+ax.XLabel.String = 'Frecuencia (Hz)';
+ax.YLabel.String = 'Fase (grados)';
 
 
 s = tf('s');
 C = 12e-9;
 L = 1000e-6;
 R = 120;
-H = (s*C*R)/(s^2*L*C + s*C*R+1);
+H = (1)/(s^2*L*C + s*C*R+1);
 
 figure;
 bode(H)
